b66a5e80940ab5403834b7f26a38bfa5d01e04fa
Nole <universe_fly@icloud.com>
2021-06-01 19:54:55 +0800

    Add a way to dump seeds generated by tvmfuzz to binary files

---
+++ src/dump_seeds.py
@@ -0,0 +1,35 @@
+"""
+Generate TIRs and stores them as seeds
+"""
+
+from typing import Tuple, List
+
+from tvm import tir
+from symboltable import SymbolTable
+from expr_generation import generate_tvm_and_np_tree
+
+import pickle
+import argparse
+import os
+
+# specify the number of seeds wanna generate and the directory to store them
+parser = argparse.ArgumentParser()
+parser.add_argument('--number', '-n', type=int, default=100)
+parser.add_argument('--dir-to-store', '-d', type=str, default='seeds')
+args = parser.parse_args()
+
+# Returns a pair of TIR and its parameter variables(IMPORTANT)
+def gen_tir_with_vars() -> Tuple[tir.expr.PrimExpr, List[tir.expr.Var]]:
+    SymbolTable.variables = []
+    generated_tir: tir.expr.PrimExpr = generate_tvm_and_np_tree().emit_tvm()
+    variables: List[tir.expr.Var] = SymbolTable.variables
+    return (generated_tir, variables)
+
+if __name__ == '__main__':
+    if not os.path.exists(args.dir_to_store):
+        os.makedirs(args.dir_to_store)
+        print(f"Successfully create directory: {args.dir_to_store}")
+    for index in range(args.number):
+        with open(os.path.join(args.dir_to_store, str(index)), 'wb') as f:
+            data = gen_tir_with_vars()
+            pickle.dump(data, f)
